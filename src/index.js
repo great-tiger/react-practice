e389bcd4103206379de12b6e941054bb57670f22
chenlihu <575569365@qq.com>
2017-04-28 17:25:47 +0800

    测试使用combineReducers

--- src/index.js
+++ src/index.js
@@ -1,50 +1,55 @@
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
-import {Provider,connect} from 'react-redux'
+import { createStore,combineReducers } from 'redux'
+import { Provider, connect } from 'react-redux'
 import DevTools from './containers/DevTools/DevTools.js'
 
-const changeText="changeText";
-
-function reducer(state={txt:'哈哈'},action){
-	if(action.type==changeText){
-		return {...state,txt:action.txt}
-	}
-	return {...state}
+const changeText = "changeText";
+const initialState = {
+    txt: '哈哈'
+};
+//注意：这里传入空对象，也不会用默认值的
+function reducer(state = initialState, action) {
+    console.log('reducer', state);
+    if (action.type == changeText) {
+        return {...state, txt: action.txt }
+    }
+    return {...state }
 }
 
-function change(txt){
-	return {
-		type:changeText,
-		txt:txt
-	}
+let rootReducer=combineReducers({
+	test:reducer
+});
+
+function change(txt) {
+    return {
+        type: changeText,
+        txt: txt
+    }
 }
 
-let store=createStore(reducer,{},DevTools.instrument());
+let store = createStore(rootReducer,{}, DevTools.instrument());
 
-@connect(state=>({txt:state.txt}))
-class App extends React.Component{
-	changeText(){
-		var val=this.refs.txt.value;
-		this.props.dispatch(change(val))
-	}
+@connect(state => ({ txt: state.test.txt }))
+class App extends React.Component {
+    changeText() {
+        var val = this.refs.txt.value;
+        this.props.dispatch(change(val))
+    }
 
-	render(){
-		return <div>
+    render() {
+        return <div>
 				  <div>{this.props.txt}</div>
 				  <div><input type="text" ref="txt" /></div>
 				  <div><input type="button" value="变变变" onClick={this.changeText.bind(this)} /></div>
 			   </div>
-	}
+    }
 }
 
 
 ReactDOM.render(
-	<Provider store={store}>
+    <Provider store={store}>
 		<div>
 	        <App />
 	        <DevTools />
 	    </div>
-	</Provider>
-,document.getElementById('container'));       
-
-
+	</Provider>, document.getElementById('container'));
